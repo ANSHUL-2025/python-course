baabc99e06421af4fdaeef46f91058c744b9fd8b
anshul:RAJ <skb.sumankhadbhandar@gmail.com>
2025-06-12 20:42:00 +0530

    Add files via upload

---
+++ Intro.py
@@ -0,0 +1,12 @@
+#Intoduction to python
+print("Anshul raj")
+message = "Hello, World!"
+print(message)
+
+def greet(name):
+    """This is a multi-line comment
+    This function greets the person passed in as a parameter
+    """
+    print("Hello, " + name + "!")
+
+greet("User")
