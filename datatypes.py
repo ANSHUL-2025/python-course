ae5563562aa431339e011b396285a5b1cca5d160
anshul:RAJ <skb.sumankhadbhandar@gmail.com>
2025-06-19 20:42:06 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,27 @@
+#input a word 
+text = str(input("Enter a string: "))
+
+# Reverse String
+# using step value as -1 iterate in reverse
+revtext = text[::-1]
+text = revtext
+
+print("Reverse of Given String:")
+print(text)
+
+
+#Let's check datatype of diffrent values
+a=5
+print(type(a))
+z=float (a)
+print(type(z))
+b=3.9
+print(type(b))
+x=int
+print(type(x))
+c= "coding"
+print(type(c))
+d= True
+print(type(d))
+
+
---
+++ keywords.py
@@ -0,0 +1,7 @@
+a=25
+print(a)
+b="hello codingal"
+print(b)
+import keyword
+
+print(keyword.kwlist)
